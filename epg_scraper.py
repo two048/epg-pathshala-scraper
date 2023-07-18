e69b0d7520fa55cd7acbfbd26ea28d3c2661c2c3
Bhanu <bhanups.2048@gmail.com>
2023-07-18 11:42:34 +0530

    fixed some bugs

--- epg_scraper.py
+++ epg_scraper.py
@@ -11,14 +11,16 @@ driver.get(url)
 
 select = Select(driver.find_element(by=By.ID, value="Paper"))
 
+not_downloaded = []
+
 for i, j in zip(range(0,len(select.options)), select.options):
     if i == 0:
         continue
-
+    
     select.select_by_index(i)
-    time.sleep(2)
+    time.sleep(5)
     select2 = Select(driver.find_element(by=By.ID, value="Module"))
-    print(select.options[i].text)
+    print("Created Folder: ", select.options[i].text)
 
     if not os.path.exists(select.options[i].text):
         os.mkdir(select.options[i].text)
@@ -28,12 +30,46 @@ for i, j in zip(range(0,len(select.options)), select.options):
             continue
 
         select2.select_by_index(k)
-        time.sleep(4)
-        src = driver.find_element(by=By.ID, value="ifrmet")\
-            .find_element(by=By.TAG_NAME, value="iframe")\
-            .get_attribute("src")
-        
-        print(f"\t{select2.options[k].text}\n\t\t{src}\n")
+        time.sleep(5)
+        try:
+            src = driver.find_element(by=By.ID, value="ifrmet")\
+                .find_element(by=By.TAG_NAME, value="iframe")\
+                .get_attribute("src")
+        except:
+            print("Reattempting 1...")
+            time.sleep(5)
+
+            try:
+                src = driver.find_element(by=By.ID, value="ifrmet")\
+                    .find_element(by=By.TAG_NAME, value="iframe")\
+                    .get_attribute("src")
+            except:
+                print("Reattempting 2...")
+                time.sleep(5)
+                try:
+                    src = driver.find_element(by=By.ID, value="ifrmet")\
+                        .find_element(by=By.TAG_NAME, value="iframe")\
+                        .get_attribute("src")
+                except:
+                    print("Reattempting 3...")
+                    time.sleep(10)
+                    try:
+                        src = driver.find_element(by=By.ID, value="ifrmet")\
+                            .find_element(by=By.TAG_NAME, value="iframe")\
+                            .get_attribute("src")
+                    except:
+                        print("Reattempting 4...")
+                        time.sleep(10)
+                        try:
+                            src = driver.find_element(by=By.ID, value="ifrmet")\
+                                .find_element(by=By.TAG_NAME, value="iframe")\
+                                .get_attribute("src")
+                        except:
+                            print(f"Could not Download: \t{select2.options[k].text}\n\t\t{src}\n")
+                            not_downloaded.append((select.options[i].text, select2.options[k].text))
+                            continue
+                    
+        print(f"Downloading:\t{select2.options[k].text}\n\t\t{src}\n")
 
         file = requests.get(src)
         name = select2.options[k].text
@@ -42,3 +78,7 @@ for i, j in zip(range(0,len(select.options)), select.options):
             f.write(file.content)
 
 driver.quit()
+
+with open("not_downloaded.txt", "w") as f:
+    for i in not_downloaded:
+        f.write(f"{i[0]}\t{i[1]}\n")
